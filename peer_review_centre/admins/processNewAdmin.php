adb2af7ef31a73da31df2bf83c1c39f9b0aa0d27
Paula Lopez Pozuelo <plopezpozuelo@gmail.com>
2015-02-22 18:53:19 +0000

    Added password confirmation to admin form

--- peer_review_centre/admins/processNewAdmin.php
+++ peer_review_centre/admins/processNewAdmin.php
@@ -3,15 +3,23 @@
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL | E_STRICT);
 
-	include 'admin.php';
+	if ($_POST["password"] == $_POST["confirmPassword"]) {
 
-	// Create a new Admin object from information in the form
-	$newAdmin = new Admin($_POST["firstName"], $_POST["lastName"], $_POST["email"], $_POST["password"]);
+		include 'admin.php';
 
-	// Create an insert query to send to mySQL and check that it isn't null
-	if (($query = $newAdmin->createInsertQuery()) != null){
-		include '../connect_to_db.php';
-		$result = $conn->query($query);
-		$conn->close();
+		// Create a new Admin object from information in the form
+		$newAdmin = new Admin($_POST["firstName"], $_POST["lastName"], $_POST["email"], $_POST["password"]);
+
+		// Create an insert query to send to mySQL and check that it isn't null
+		if (($query = $newAdmin->createInsertQuery()) != null){
+			include '../connect_to_db.php';
+			$result = $conn->query($query);
+			$conn->close();
+		}
+
+		echo "Registration successful.";
+
+	} else {
+		echo "Passwords do not match, please try again.";
 	}
 ?>
