4ce171c34b155f2f4d1505cd6901c17385c2d7e2
axstw <a.striesow@gmx.de>
2015-02-27 19:13:50 +0000

    forum class

---
+++ peer_review_centre/forum.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*/
+class Forum {
+
+  var $groupID;
+  // Variable adminID will be handled by sql (automatically generated)
+
+  function __construct($groupID) {
+    this->groupID = groupID;
+  }
+
+  function createInsertQuery(){
+    // Create a string that is a MYSQL INSERT query
+    $query = "INSERT INTO forums (forumID, groupID) VALUES (NULL, '$groupID');";
+    return $query;
+  }
+
+}
+
+?>
