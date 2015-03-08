1f4320c1cb08c405f365497412766234661cca17
Sami Start <samistart@hotmail.com>
2015-03-08 22:55:57 +0000

    added password hashing for students and admins controller

--- peer_review_centre/controller/process_new_admin.php
+++ peer_review_centre/controller/process_new_admin.php
@@ -3,21 +3,31 @@
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL | E_STRICT);
 
-	//Sanitize to prevent naughty hackers from putting HTML tags
-	$firstName = filter_var($_POST['firstName'], FILTER_SANITIZE_STRING);
-	$lastName = filter_var($_POST['lastName'], FILTER_SANITIZE_STRING);
-	$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
 	if ($_POST["password"] == $_POST["confirmPassword"]) {
 		//Check if the email is valid using built-in php function    
-		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+
+			//Sanitize to prevent naughty hackers from putting HTML tags
+			//mysql_escape_string is tested here but needs to be replaced with mysqli_real_escape_string(connection,escapestring)
+			$firstName = mysql_escape_string(filter_var($_POST['firstName'], FILTER_SANITIZE_STRING));
+			$lastName = filter_var($_POST['lastName'], FILTER_SANITIZE_STRING);
+			$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+			//Use the PHP 5.5 hashing API. To check password use
+			// if (password_verify($password, $hashAndSalt)) {
+		 	//   // Verified
+			// }
+			// See http://stackoverflow.com/questions/14992367/using-php-5-5s-password-hash-and-verify-function-am-i-doing-it-right
+			$hashAndSalt = password_hash($_POST["password"], PASSWORD_BCRYPT);	
+
+
 		  // Create a new Admin object from information in the form (groupID = 1 for now)
-			    $newAdmin = new Admin();
-			    $newAdmin->firstName = $firstName; 
-			    $newAdmin->lastName = $lastName;
-			    $newAdmin->email = $email;
-			    $newAdmin->password = $_POST["password"];
-			    $newAdmin->create();
+	    $newAdmin = new Admin();
+	    $newAdmin->firstName = $firstName; 
+	    $newAdmin->lastName = $lastName;
+	    $newAdmin->email = $email;
+	    $newAdmin->password = $hashAndSalt;
+	    $newAdmin->create();
 
 			    echo "Registration successful.";
 		}
--- peer_review_centre/controller/process_new_student.php
+++ peer_review_centre/controller/process_new_student.php
@@ -3,24 +3,32 @@
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL | E_STRICT);
 
-	//Sanitize to prevent naughty hackers from putting HTML tags
-	//mysql_escape_string is tested here but needs to be replaced with mysqli_real_escape_string(connection,escapestring)
-	$firstName = mysql_escape_string(filter_var($_POST['firstName'], FILTER_SANITIZE_STRING));
-	$lastName = filter_var($_POST['lastName'], FILTER_SANITIZE_STRING);
-	$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-
 
 	if ($_POST["password"] == $_POST["confirmPassword"]) {
 		//Check if the email is valid using built-in php function    
-		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+
+			//Sanitize to prevent naughty hackers from putting HTML tags
+			//mysql_escape_string is tested here but needs to be replaced with mysqli_real_escape_string(connection,escapestring)
+			$firstName = mysql_escape_string(filter_var($_POST['firstName'], FILTER_SANITIZE_STRING));
+			$lastName = filter_var($_POST['lastName'], FILTER_SANITIZE_STRING);
+			$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+			//Use the PHP 5.5 hashing API. To check password use
+			// if (password_verify($password, $hashAndSalt)) {
+		 	//   // Verified
+			// }
+			// See http://stackoverflow.com/questions/14992367/using-php-5-5s-password-hash-and-verify-function-am-i-doing-it-right
+			$hashAndSalt = password_hash($_POST["password"], PASSWORD_BCRYPT);	
+
+
 		  // Create a new Student object from information in the form (groupID = 1 for now)
-			    $newStudent = new Student();
-			    $newStudent->firstName = $firstName; 
-			    $newStudent->lastName = $lastName;
-			    $newStudent->email = $email;
-			    $newStudent->password = $_POST["password"];
-			    $newStudent->groupID = 1;
-			    $newStudent->create();
+	    $newStudent = new Student();
+	    $newStudent->firstName = $firstName; 
+	    $newStudent->lastName = $lastName;
+	    $newStudent->email = $email;
+	    $newStudent->password = $hashAndSalt;
+	    $newStudent->groupID = "1";
+	    $newStudent->create();
 
 			    echo "Registration successful.";
 		}
