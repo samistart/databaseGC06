1f17f62744823b4c7da82e371bfddc1501ce2eed
Paula Lopez Pozuelo <plopezpozuelo@gmail.com>
2015-03-09 15:57:16 +0000

    Modified assessment.php and deleted groupReportAssessment.php (to match the new database structure)

--- peer_review_centre/model/assessment.php
+++ peer_review_centre/model/assessment.php
@@ -10,27 +10,22 @@ class Assessment extends DatabaseObject{
 	// Name of the corresponding database table and fields are stored in static variables.
 	// (will be common to every instance of the class)
 	protected static $table_name='assessments';
-	protected static $db_fields = array('assessmentID', 'criteria', 'comment', 'grade');
+	protected static $db_fields = array('assessmentID', 'groupID', 'reportID', 'criteria1', 'comment1', 'grade1', 'criteria2', 'comment2', 'grade2', 'criteria3', 'comment3', 'grade3');
 
 	// Variables that correspond to the fields of the corresponding table, that will be given values
 	// for each object (creating the corresponding table entry)
 	public $assessmentID = "NULL";
-	public $criteria;
-	public $comment;
-	public $grade;
-
-
-	/**
-	*A constructor taking args for use in taking form data and create a new object.
-	*/
-	function __construct($criteria, $comment, $grade){
-		
-		$this->criteria = $criteria;
-		$this->comment = $comment;
-		$this->grade = $grade;
-		//set the default value for groupId as 1 for now as a test (because there is only groupID 1 in the table)
-		//assessmentId and lastEdited will be automatically filled out (AI and timestamp)
-	}
+	public $groupID;
+	public $reportID;
+	public $criteria1;
+	public $comment1;
+	public $grade1;
+	public $criteria2;
+	public $comment2;
+	public $grade2;
+	public $criteria3;
+	public $comment3;
+	public $grade3;
 
 	/**
 	* Method that returns the variable that corresponds to the primary key.
