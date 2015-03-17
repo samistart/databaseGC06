6221f147b2001606e814e40b9ddcfd1b7e69d753
axstw <a.striesow@gmx.de>
2015-03-17 01:04:27 +0000

    admin header links updated.

--- peer_review_centre/layouts/admin_navbar.php
+++ peer_review_centre/layouts/admin_navbar.php
@@ -2,8 +2,8 @@
     <ul> 
       <li><a href='<?php echo WEB_ROOT; ?>views/prc_admin/admins/index.php'>Home</a></li>
       <li><a href='<?php echo WEB_ROOT; ?>views/prc_admin/groups/index.php'>Groups</a></li>
-      <li><a href='<?php echo WEB_ROOT; ?>views/prc_admin/groups/allocate.php'>Allocation</a></li>
-      <li><a href='#'>Blah</a></li>
+      <li><a href='<?php echo WEB_ROOT; ?>views/prc_admin/groups/allocate.php'>Group Allocation</a></li>
+      <li><a href='<?php echo WEB_ROOT; ?>views/prc_admin/assessments/assign.php'>Assessment Assignment</a></li>
       <li><a href='<?php echo WEB_ROOT; ?>controllers/prc_admin/admin_logout.php'>Logout</a></li> 
     </ul>   
 </div>
