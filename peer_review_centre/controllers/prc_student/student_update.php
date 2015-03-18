213ac93e2e577bdc3c60d9aab4cd1e5249df7002
Sami Start <samistart@hotmail.com>
2015-03-18 20:14:56 +0000

    fixed bug in update controller

--- peer_review_centre/controllers/prc_student/student_update.php
+++ peer_review_centre/controllers/prc_student/student_update.php
@@ -5,24 +5,31 @@ error_reporting(E_ALL | E_STRICT);
 
 require_once("../../includes/initialise_student.php");
 
-if ($_POST["newPassword"] == $_POST["confirmPassword"]) {
-    $currentStudent = Student::findByID("$session->userID");
-    $email = $currentStudent->email;
-    //Use the PHP 5.5 hashing API. To check password use
-    // if (password_verify($password, $hashAndSalt)) {
-    //   // Verified
-    // }
+if (empty($_POST["oldPassword"]) || empty($_POST["newPassword"]) || empty($_POST["confirmPassword"])) {
+    $session->errorMessage("Please fill out all fields.");
+    redirectTo("views/prc_student/students/update.php");
+}
 
-    $foundStudent = Student::authenticate($email, $_POST["oldPassword"]);  
+if ($_POST["newPassword"] != $_POST["confirmPassword"]) {
+    $session->errorMessage("Passwords do not match.");
+    redirectTo("views/prc_student/students/update.php");
+}
 
+$currentStudent = Student::findByID("$session->userID");
+$email = $currentStudent->email;
+
+$foundStudent = Student::authenticate($email, $_POST["oldPassword"]); 
+
+if ($foundStudent) {
     $foundStudent->password = password_hash($_POST["newPassword"], PASSWORD_BCRYPT);
     $foundStudent->update();
 
     $session->message("Your password was updated successfully.");
     redirectTo("views/prc_student/students/index.php");
 } else {
-    $session->errorMessage("Passwords do not match.");
-    redirectTo("views/prc_student/students/create.php");
+    $session->errorMessage("Incorrect password.");
+    redirectTo("views/prc_student/students/update.php"); 
 }
+
 
 ?>
