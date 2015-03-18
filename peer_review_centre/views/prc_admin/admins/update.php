9a90715a8137f6e20552b1f432e49018afc968d8
Paula Lopez Pozuelo <plopezpozuelo@gmail.com>
2015-03-18 19:48:01 +0000

    Bootstrapped password update pages

--- peer_review_centre/views/prc_admin/admins/update.php
+++ peer_review_centre/views/prc_admin/admins/update.php
@@ -11,21 +11,45 @@
   $createAdmin = WEB_ROOT."views/prc_admin/admins/create.php";
 ?>
 
-<h2>Change Password</h2>
+<div class="container" style="width:70%;">
+  <div class="jumbotron">
+    <legend>
+      <h2>Change Password</h2>
+    </legend>
+    
+    <!-- Display possible success message in green box -->
+    <?php if (($session->message)) {greenBox($session->message);} ?>
+    <!-- Display possible error message in red box -->
+    <?php if (($session->errorMessage)) {redBox($session->errorMessage);} ?>
 
-<!-- Display possible success message in green box -->
-<?php if (($session->message)) {greenBox($session->message);} ?>
-<!-- Display possible error message in red box -->
-<?php if (($session->errorMessage)) {redBox($session->errorMessage);} ?>
-
-<form method='post' action='<?php echo $action; ?>' name='newUser'>
-  <label>Old password:</label>
-  <input type='password' name='oldPassword' size='30'><br>
-  <label>New password:</label>
-  <input type='password' name='newPassword' size='30'><br>
-  <label>Confirm new password:</label>
-  <input type='password' name='confirmPassword' size='30'>
-  <p><input type='submit' value='Submit changes'></p>
-</form>
+    <form class="form-horizontal" method='post' action='<?php echo $action; ?>' name='changePassword'>
+      <fieldset>
+      <div class="form-group">
+        <label for="inputEmail" class="col-lg-2 control-label">Old password</label>
+        <div class="col-lg-10">
+          <input type="password" class="form-control" name="oldPassword">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail" class="col-lg-2 control-label">New password</label>
+        <div class="col-lg-10">
+          <input type="password" class="form-control" name="newPassword">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputPassword" class="col-lg-2 control-label">Confirm new password</label>
+        <div class="col-lg-10">
+            <input type="password" class="form-control" name="confirmPassword">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-10 col-lg-offset-2">
+          <button type="submit" value='Submit changes' class="btn btn-primary">Change Password</button>
+        </div>
+      </div>
+      </fieldset>
+    </form>
+  </div>
+</div>
 
 <?php include SITE_ROOT.DS.'layouts/footer.php';?>
