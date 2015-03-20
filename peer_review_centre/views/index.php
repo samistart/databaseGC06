f875116e5f6b7acdce33213209f115fce125957b
samistart <samistart@hotmail.com>
2015-03-20 22:06:11 +0000

    fixed the landing page with picture background

--- peer_review_centre/views/index.php
+++ peer_review_centre/views/index.php
@@ -1,4 +1,7 @@
 <?php 
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL | E_STRICT);
+
   // Initialise student files and verify whether a student user is logged in.
   require_once('../includes/initialise_student.php');
   InitialiseStudent::reverseCheckLoggedIn();
@@ -7,15 +10,18 @@
 <br> <br> <br>
 
 <!-- Jumbotron container with welcome page -->
-<div class ="container" styled="width:50%">
-  <div class="jumbotron">
-    <h1>Peer Review Center</h1>
-    <p>Welcome! Start by registering for an account or sign into an existing one.</p>
-    <p>
-      <a class="btn btn-primary btn-lg" href="prc_student/students/create.php">Register</a>
-      <a class="btn btn-primary btn-lg" href="prc_student/students/login.php">Login</a>
-    </p>
-  </div>
+<div class="fullwidth">
+    <div class ="container" styled="width:50%">
+      <div class="jumbotron" style="background: transparent;">
+        <h1 style="color:white;">Peer Review Center</h1>
+        <p style="color:white;">Welcome! Get started by registering for an account below.</p>
+        <p>
+          <a class="btn btn-primary btn-lg" href="prc_student/students/create.php">Register</a>
+          <a class="btn btn-primary btn-lg" href="prc_student/students/login.php">Login</a>
+        </p>
+      </div>
+    </div>
 </div>
 
 <?php include SITE_ROOT.DS.'layouts/footer.php';?>
+
