167eba27592d57da55ff01db1bb9d262902e0321
samistart <samistart@hotmail.com>
2015-03-20 22:10:48 +0000

    Revert "fixed the landing page with picture background" Reverted this change due to the unpopularity of the doge picture This reverts commit f875116e5f6b7acdce33213209f115fce125957b.

--- peer_review_centre/views/index.php
+++ peer_review_centre/views/index.php
@@ -1,7 +1,4 @@
 <?php 
-  ini_set('display_errors', 'On');
-  error_reporting(E_ALL | E_STRICT);
-
   // Initialise student files and verify whether a student user is logged in.
   require_once('../includes/initialise_student.php');
   InitialiseStudent::reverseCheckLoggedIn();
@@ -10,18 +7,15 @@
 <br> <br> <br>
 
 <!-- Jumbotron container with welcome page -->
-<div class="fullwidth">
-    <div class ="container" styled="width:50%">
-      <div class="jumbotron" style="background: transparent;">
-        <h1 style="color:white;">Peer Review Center</h1>
-        <p style="color:white;">Welcome! Get started by registering for an account below.</p>
-        <p>
-          <a class="btn btn-primary btn-lg" href="prc_student/students/create.php">Register</a>
-          <a class="btn btn-primary btn-lg" href="prc_student/students/login.php">Login</a>
-        </p>
-      </div>
-    </div>
+<div class ="container" styled="width:50%">
+  <div class="jumbotron">
+    <h1>Peer Review Center</h1>
+    <p>Welcome! Start by registering for an account or sign into an existing one.</p>
+    <p>
+      <a class="btn btn-primary btn-lg" href="prc_student/students/create.php">Register</a>
+      <a class="btn btn-primary btn-lg" href="prc_student/students/login.php">Login</a>
+    </p>
+  </div>
 </div>
 
 <?php include SITE_ROOT.DS.'layouts/footer.php';?>
-
